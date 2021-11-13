f562f4db60b3fd399f714211c29f9b970429e0be
Darko Novak <novak.darko1@gmail.com>
2021-11-13 07:24:47 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, make_response
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    weather_info = "Danes je zelo lep dan."
+    current_year = datetime.datetime.now().year
+
+    cities = ["Ljubljana", "Celje", "Maribor", "Ptuj", "Koper"]
+    return render_template("index.html", weather_info = weather_info, current_year = current_year, cities = cities)
+
+@app.route("/about", methods=["GET", "POST"])
+def about():
+    if request.method == "GET":
+        name = request.cookies.get("name")
+        return render_template("about.html", name=name)
+    elif request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+
+        print(name)
+        print(email)
+        print(message)
+
+        response = make_response(render_template("success.html"))
+        response.set_cookie("name", name)
+        return response
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+if __name__ == "__main__":
+    app.run(use_reloader=True, debug=True)
